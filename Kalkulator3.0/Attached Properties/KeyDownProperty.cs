029335ab5e01b7dc3e8dc5ba8ba71d44cf47fe91
Hooterr <maxusard@gmail.com>
2017-10-06 10:40:34 +0200

    Added attached properties

---
+++ Kalkulator3.0/Attached Properties/KeyDownProperty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Kalkulator3
+{
+    /// <summary>
+    /// The NoFrameHistory attached property for creating a <see cref="Frame"/> that never shows navigation
+    /// and keeps the navigation history empty
+    /// </summary>
+    public class KeyDownProperty : BaseAttachedProperty<KeyDownProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Get the window
+            var Window = (sender as Window);
+            Window.Loaded += ((ss, ee) => { (ss as Window).Focus(); });
+            Window.KeyDown += ((ss, ee) => { MessageBox.Show(ee.Key.ToString()); });
+
+        }
+    }
+}
